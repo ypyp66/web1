9bd5564369e2a07a024f232115d903abcca1e377
ypyp66 <57565933+ypyp66@users.noreply.github.com>
2019-12-07 02:36:07 +0900

    1

---
+++ login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ko" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo $_GET['id']."님 안녕하세요<br>";
+    ?>
+  </body>
+</html>
